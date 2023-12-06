143e5b25b688cdb34b774b1355141c7cbc28ce19
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2023-12-06 17:55:12 +0100

    feat(init): initial commit of kindergarden app nr8 after lecture
    includes pagination

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DataComponent } from './dashboard/data/data.component';
+import { AddDataComponent } from './dashboard/add-data/add-data.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './dashboard/button/button.component';
+import { PaginationPipe } from './dashboard/data/pagination.pipe';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AddDataComponent,
+    DataComponent,
+    HeaderComponent,
+    ButtonComponent,
+    PaginationPipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatTableModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
